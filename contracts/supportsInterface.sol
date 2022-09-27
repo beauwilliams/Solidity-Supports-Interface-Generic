1037a011dc07576be801637a68c1dae057e926e6
beauwilliams <beau.williams@icloud.com>
2022-09-27 18:04:34 +1000

    feat: supports interfact contract

--- contracts/supportsInterface.sol
+++ contracts/supportsInterface.sol
@@ -2,16 +2,30 @@
 // Author: beauwilliams
 pragma solidity 0.8.7;
 
-// A generic, pure implementation to check if a token is a 721 or 1155.
-// Extendable to many interface types through the ID hash of an interface.
+// A generic, pure implemtenation to check if a contract/token supports an interface.
+// Such as being able to verify 'this token is an ERC721'
+// Extendable to any custom interface addressed the ID of an interface, calculated using below example.
 
 
-// Openzeppelin IERC721/1151 interface, which is inhereted from IERC165 uses view instead of pure.
-// You will need to override OZ to create a pure implementation if you wish like this example.
+// Most IERC721/1151 interface, which inherit from IERC165 uses view instead of pure.
+// You will need to override them to create a pure implementation if you wish like this example.
 interface IERC165 {
     function supportsInterface(bytes4 interfaceId) external pure returns (bool);
 }
 
+//Example interface, we will generate an interface ID for. This can be your own contract interface.
+interface ExampleInterface {
+    function hello() external;
+    function world() external;
+}
+
+//There may be a generic implementation of this function for all interfaces. Ideas? ;)
+//This function will return an interface ID. I.e 0x80ac58cd == IERC721
+contract InterfaceIdGenerator {
+  function genStoreInterfaceId(ExampleInterface i) external pure returns (bytes4) {
+    return i.hello.selector ^ i.world.selector;
+  }
+}
 
 abstract contract SupportsERC {
     // We can hardcode these variables into our contracts to save gas.
@@ -20,9 +34,11 @@ abstract contract SupportsERC {
     // bytes4 private constant INTERFACE_ID_ERC721 = 0x80ac58cd;
     // bytes4 private constant INTERFACE_ID_ERC1155 = 0xd9b67a26;
 
+    //@return: true if a contract supports an interface
     function supportsERC(address nftAddress, bytes4 interfaceId) external pure returns (bool) {
         return IERC165(nftAddress).supportsInterface(interfaceId);
     }
 }
 
+//Our base implementation contract for purpose of this example
 contract MyContract is SupportsERC {}
